985594dbb7859162e268647ad573c20af1ee96c2
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-06 22:48:19 -0500

    conection firbase

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                CollectionReference usersReference =
+                    FirebaseFirestore.instance.collection("users");
+                usersReference.get().then(
+                      (value) => print(value),
+                    );
+              },
+              child: Text("Traer datos"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
