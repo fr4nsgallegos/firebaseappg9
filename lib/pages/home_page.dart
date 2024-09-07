6058a2a1832add767d441149ef0ada646ea39d3e
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-07 10:06:03 -0500

    add, update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  CollectionReference usersReference =
+      FirebaseFirestore.instance.collection("users");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +13,6 @@ class HomePage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                CollectionReference usersReference =
-                    FirebaseFirestore.instance.collection("users");
                 usersReference.get().then(
                   (value) {
                     QuerySnapshot userCollection = value;
@@ -33,6 +33,61 @@ class HomePage extends StatelessWidget {
                 );
               },
               child: Text("Traer datos"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference
+                    .where("name", isEqualTo: "Jhonny")
+                    .where("age", isGreaterThan: 39)
+                    .get()
+                    .then(
+                  (value) {
+                    QuerySnapshot userCollection = value;
+                    List<QueryDocumentSnapshot> docs = userCollection.docs;
+                    docs.forEach((element) {
+                      print("--------------------------");
+                      print(element.id);
+                      print(element.data());
+                      print("--------------------------");
+                    });
+                  },
+                );
+              },
+              child: Text("Data filtrada"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.add({
+                  "name": "Andrea",
+                  "lastName": "VMengoza",
+                  "age": "36",
+                  "estatura": 169,
+                }).then((value) {
+                  print(value);
+                  print(value.id);
+                });
+              },
+              child: Text("Agrear user"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.doc("idNoExiste").set({
+                  "name": "Javier",
+                  "lastName": "Madera",
+                  "age": 65,
+                  "estatura": 168
+                });
+              },
+              child: Text("Inserción 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                usersReference.doc("idNoExiste").update({
+                  "name": "Anastacio",
+                  "nacionality": "Peruano",
+                });
+              },
+              child: Text("Actualización"),
             )
           ],
         ),
